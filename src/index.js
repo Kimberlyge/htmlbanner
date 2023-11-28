303de1c54182bcf223a3fa36c418346e4f758b94
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2023-11-29 00:42:49 +0100

    More animation for the text

--- src/index.js
+++ src/index.js
@@ -2,67 +2,79 @@
 // import { example } from '@/js/example'
 
 // Test import of styles
-import '@/styles/index.scss'
+import "@/styles/index.scss";
 // import gsap from 'gsap';
 
+const duration = 5000;
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const containerDimensions = [
-//         { dimension: '300600', totalImages: 60, fps: 29.97},
-//         { dimension: '320480', totalImages: 234, fps: 29.97 },
-//         { dimension: '300250', totalImages: 140, fps: 29.97 },
-//         { dimension: '72890', totalImages: 258, fps: 29.97  },
-//         { dimension: '97090', totalImages: 60, fps: 29.97  },
-//         { dimension: '1080', totalImages: 150, fps: 29.97 },
-//     ];
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".text__part").forEach((element) => {
+    const animationDelay = getComputedStyle(element).animationDelay;
 
-//     containerDimensions.forEach(({ dimension, totalImages, fps }) => {
-//         const container = document.getElementById(`animation-container-${dimension}`);
-//         const filenames = generateFilenames(dimension, totalImages);
+    setInterval(() => {
+      element.style.animationDelay = "0s";
+      console.log(animationDelay);
+      setTimeout(() => {
+        element.style.animationDelay = animationDelay;
+      }, 100);
+    }, duration);
+  });
 
-//         loadImagesAndAnimate(container, filenames, fps);
-//     });
+  // const containerDimensions = [
+  //     { dimension: '300600', totalImages: 60, fps: 29.97},
+  //     { dimension: '320480', totalImages: 234, fps: 29.97 },
+  //     { dimension: '300250', totalImages: 140, fps: 29.97 },
+  //     { dimension: '72890', totalImages: 258, fps: 29.97  },
+  //     { dimension: '97090', totalImages: 60, fps: 29.97  },
+  //     { dimension: '1080', totalImages: 150, fps: 29.97 },
+  // ];
 
-//     function generateFilenames(dimension, totalImages) {
-//         const filenames = [];
+  // containerDimensions.forEach(({ dimension, totalImages, fps }) => {
+  //     const container = document.getElementById(`animation-container-${dimension}`);
+  //     const filenames = generateFilenames(dimension, totalImages);
 
-//         for (let i = 1; i <= totalImages; i++) {
-//             filenames.push(`${dimension}/${dimension}-${i}.png`);
-//         }
+  //     loadImagesAndAnimate(container, filenames, fps);
+  // });
 
-//         return filenames;
-//     }
+  // function generateFilenames(dimension, totalImages) {
+  //     const filenames = [];
 
-//     function loadImagesAndAnimate(container, filenames, fps) {
-//         let imagesLoaded = 0;
+  //     for (let i = 1; i <= totalImages; i++) {
+  //         filenames.push(`${dimension}/${dimension}-${i}.png`);
+  //     }
 
-//         filenames.forEach((filename, index) => {
-//             const imgElement = new Image();
-//             imgElement.src = `/assets/images/${filename}`;
-//             imgElement.alt = 'Image';
+  //     return filenames;
+  // }
 
-//             imgElement.onload = () => {
-//                 imagesLoaded++;
+  // function loadImagesAndAnimate(container, filenames, fps) {
+  //     let imagesLoaded = 0;
 
-//                 if (imagesLoaded === filenames.length) {
-//                     // All images are loaded, perform animation
-//                     doAnimation(container, fps);
-//                 }
-//             };
+  //     filenames.forEach((filename, index) => {
+  //         const imgElement = new Image();
+  //         imgElement.src = `/assets/images/${filename}`;
+  //         imgElement.alt = 'Image';
 
-//             container.appendChild(imgElement);
-//         });
-//     }
+  //         imgElement.onload = () => {
+  //             imagesLoaded++;
 
-//     function doAnimation(container, fps) {
-//         const images = gsap.utils.toArray(`#${container.id} img`);
-//         // const fps = 24;
-//         const duration = 1 / fps;
+  //             if (imagesLoaded === filenames.length) {
+  //                 // All images are loaded, perform animation
+  //                 doAnimation(container, fps);
+  //             }
+  //         };
 
-//         const sequence = gsap.timeline({ repeat: -1 })
-//             .staggerTo(images, 0, { position: 'static', visibility: 'visible' }, duration, 0)
-//             .staggerTo(gsap.utils.toArray(`#${container.id} img:not(:last-child)`), 0, { position: 'absolute', visibility: 'hidden', immediateRender: false }, duration, duration)
-//             .set({}, {}, "+=" + duration);
-//     }
-    
-// });
+  //         container.appendChild(imgElement);
+  //     });
+  // }
+
+  // function doAnimation(container, fps) {
+  //     const images = gsap.utils.toArray(`#${container.id} img`);
+  //     // const fps = 24;
+  //     const duration = 1 / fps;
+
+  //     const sequence = gsap.timeline({ repeat: -1 })
+  //         .staggerTo(images, 0, { position: 'static', visibility: 'visible' }, duration, 0)
+  //         .staggerTo(gsap.utils.toArray(`#${container.id} img:not(:last-child)`), 0, { position: 'absolute', visibility: 'hidden', immediateRender: false }, duration, duration)
+  //         .set({}, {}, "+=" + duration);
+  // }
+});
